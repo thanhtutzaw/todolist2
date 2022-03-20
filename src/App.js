103e665671b5e9d73a865ec0cec9d618762a88f1
Than Htut Zaw <zgyi025@gmail.com>
2022-03-20 19:27:28 +0630

    todolistv2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, settodos] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    if (input) {
+      console.log("submit");
+      settodos([
+        ...todos,
+        { id: Math.floor(Math.random() * 10000), text: input },
+      ]);
+    } else {
+      return null;
+    }
+    setInput("");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <div onClick={submitHandle} className="clickDiv">
+          <h3 style={{ color: "white" }}>click div</h3>
+        </div> */}
+        <form onSubmit={submitHandle}>
+          <input
+          onBlur={submitHandle}
+            onChange={handleChange}
+            value={input}
+            type="text"
+            autoFocus
+            autoComplete="off"
+            spellCheck="false"
+            placeholder="HELLO"
+          />
+          {/* <button type='submit'>Submit</button> */}
+          {/* <h2>{input}</h2> */}
+        </form>
+        <div className="todoParent">
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo.text}</li>;
+          })}
+        </ul>
+        </div>
       </header>
     </div>
   );
