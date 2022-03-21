bfa1dbfb6066ffae079bfe7a0185c1fe00c25621
Than Htut Zaw <zgyi025@gmail.com>
2022-03-21 11:57:54 +0630

    added border animation when summit done and border buttom removed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function App() {
   const [input, setInput] = useState("");
   const [todos, settodos] = useState([]);
-
+const [color, setcolor] = useState(false)
   const handleChange = (e) => {
     setInput(e.target.value);
     console.log(e.target.value);
@@ -13,23 +13,37 @@ function App() {
   const submitHandle = (e) => {
     e.preventDefault();
     if (input) {
-      console.log("submit");
+        setcolor(!color)
+        setTimeout(() => setcolor(false), 1000);
+
+      // console.log("submit");
+      // let interval =setInterval( () => {
+      //   setcolor(!color)
+        
+      // }, 1000);
+      // clearInterval(interval);  
       settodos([
         ...todos,
         { id: Math.floor(Math.random() * 10000), text: input },
       ]);
+      
+      
+      
     } else {
       return null;
     }
     setInput("");
   };
   return (
-    <div className="App">
+    
+
+<div  className={`App `}>
       <header className="App-header">
         {/* <div onClick={submitHandle} className="clickDiv">
           <h3 style={{ color: "white" }}>click div</h3>
         </div> */}
         <form onSubmit={submitHandle}>
+          <div className={`inputParent ${color? 'color' : ''} `}>
           <input
           onBlur={submitHandle}
             onChange={handleChange}
@@ -40,10 +54,11 @@ function App() {
             spellCheck="false"
             placeholder="HELLO"
           />
+          </div>
           {/* <button type='submit'>Submit</button> */}
           {/* <h2>{input}</h2> */}
         </form>
-        <div onMouseLeave={ ()=>{alert('hey')}} className="todoParent">
+        <div className="todoParent">
         <ul>
           {todos.map((todo) => {
             return <li>{todo.text}</li>;
@@ -52,6 +67,7 @@ function App() {
         </div>
       </header>
     </div>
+
   );
 }
 
